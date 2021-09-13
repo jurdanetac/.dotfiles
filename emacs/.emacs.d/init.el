e900ec314fa83bf4e038543f014d6b60d616fff7
Juan Urdaneta <jurdanetac10@gmail.com>
2021-09-13 17:33:29 -0400

    Play with `evil`, garbage collection and change theme

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -5,18 +5,7 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+(require 'org)
 (org-babel-load-file
  (expand-file-name "settings.org"
                    user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (dashboard doom-modeline doom-themes evil))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
