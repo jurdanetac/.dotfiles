d70afece6d364446584de0b38c8c86273444ed54
Juan Urdaneta <jurdanetac10@gmail.com>
2021-09-14 21:13:00 -0400

    Update `org` to v9.4.6 and fix defaults, start working in the README

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -9,3 +9,17 @@
 (org-babel-load-file
  (expand-file-name "settings.org"
                    user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (org-plus-contrib which-key use-package undo-tree smartparens org-bullets modus-themes magit flycheck evil-collection doom-themes doom-modeline dashboard))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
