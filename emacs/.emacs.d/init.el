9d3a491d436046dfacb23fa26b040f2269cb9d91
Juan Urdaneta <jurdanetac10@gmail.com>
2021-09-15 21:40:01 -0400

    `helm`, and more tweaks, organize org-mode settings

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,25 +1,4 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
 (require 'org)
 (org-babel-load-file
  (expand-file-name "settings.org"
                    user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (org-plus-contrib which-key use-package undo-tree smartparens org-bullets modus-themes magit flycheck evil-collection doom-themes doom-modeline dashboard))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
