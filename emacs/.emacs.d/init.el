2af0aba5fa8fd1683265ff83a2c7f2150090c7d5
Juan Urdaneta <jurdanetac10@gmail.com>
2026-08-01 11:02:40 -0400

    Add indent buffer function to Emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,13 +1,33 @@
-;; UI Config
+;; UI
 (setq inhibit-startup-message t) ; Drop in to *scratch*
-(scroll-bar-mode -1)        ; Disable visible scrollbar
-(tool-bar-mode -1)          ; Disable the toolbar
-(tooltip-mode -1)           ; Disable tooltips
-(set-fringe-mode 10)        ; Give some breathing room
-(menu-bar-mode -1)            ; Disable the menu bar
-(setq visible-bell t)         ; Set up the visible bell
+(scroll-bar-mode -1)             ; Disable visible scrollbar
+(tool-bar-mode -1)               ; Disable the toolbar
+(tooltip-mode -1)                ; Disable tooltips
+(set-fringe-mode 10)             ; Give some breathing room
+(menu-bar-mode -1)               ; Disable the menu bar
+(setq visible-bell t)            ; Set up the visible bell
+;; UI END
 
-;; Packages
+;; FUNCTIONS
+(defun indent-whole-buffer ()
+  "Indent the entire buffer without affecting point or mark."
+  (interactive)
+  (save-excursion
+    (save-restriction
+      (indent-region (point-min) (point-max)))))
+
+(global-set-key (kbd "C-c i") 'indent-whole-buffer)
+
+(defun open-init-file ()
+  "Open Emacs configuration file, silently following symlinks."
+  (interactive)
+  (let ((vc-follow-symlinks t))  ; Temporarily set to true only inside this block
+    (find-file user-init-file)))
+
+(global-set-key (kbd "C-c e") 'open-init-file)
+;; FUNCTIONS END
+
+;; PACKAGES
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
@@ -31,11 +51,11 @@
    '(git-gutter:deleted-sign "--")
    )
 
-  (set-face-foreground 'git-gutter:added "green") ;; foreground color
+  (set-face-foreground 'git-gutter:added "green")
   (set-face-foreground 'git-gutter:modified "yellow")
   (set-face-foreground 'git-gutter:deleted "red")
 
-  (set-face-background 'git-gutter:added nil) ;; background color
+  (set-face-background 'git-gutter:added nil)
   (set-face-background 'git-gutter:modified nil)
   (set-face-background 'git-gutter:deleted nil)  
 
@@ -46,3 +66,4 @@
 
 (use-package yaml-mode
   :ensure t)
+;; PACKAGES END
